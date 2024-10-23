de3a9411dbb7049cedba31bf0e05d0b2dc74e20b
pyiah <120746250+pyiah@users.noreply.github.com>
2024-10-22 23:36:28 -0400

    Rem. Mondex func called, defined list of mons
    Removed my calling of the Mondex func and defined a future scalable function that prints our list of monsters.

--- d_pokemon.py
+++ d_pokemon.py
@@ -10,19 +10,24 @@ class Monster:
         self.health_stat = health_stat
         self.monType = monType
         
-    def Mondex(self):
+    def Mondex(self, ID=None): #Method for getting all of our properties of a monster object
         print("==========MONDEX==========")
         print("ID:", self.monID)
         print("Monster:", self.name)
-        print("Moves:\n", list(self.moves.values())[0][0]," --> ", list(self.moves.values())[1], "Type\n")
+        print("Moves:\n", list(self.moves.values())[0][0]," --> ", list(self.moves.values())[1][0], "Type\n")
         print("Attack Stat:", self.attack_stat)
         print("Defense stat:", self.defense_stat)
         print("Health stat:", self.health_stat)
         print("Typing:", self.monType)
         
 
+Orgo = Monster("Orgo", {"Moves": ["Punch"], "Type": ["Fire"]}, monID=1, attack_stat=10, defense_stat=11, health_stat=12, monType="Fire")
+Borg = Monster("Borg", {"Moves": ["Smack"], "Type": ["Water"]}, monID=2, attack_stat=10, defense_stat=11, health_stat=12, monType="Water")
 
-Orgo = Monster("Orgo", {"Moves": ["Punch"], "Type": "Fire"}, monID=1, attack_stat=10, defense_stat=11, health_stat=12, monType="Fire")
-Borg = Monster("Borg", {"Moves": ["Smack"], "Type": "Water"}, monID=2, attack_stat=10, defense_stat=11, health_stat=12, monType="Water")
+monster_list = [Orgo, Borg]
 
-Orgo.Mondex()
+def list_of_monsters(): #Allows for a list of monsters to be printed to the console. Will be used with the Mondex for future code.
+    number = 1
+    for monsters in monster_list:
+        print(str(number) + ".", monsters.name)
+        number += 1
